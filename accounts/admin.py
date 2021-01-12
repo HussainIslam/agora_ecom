3653cffbed63c5972b62936722e2d33c1cf15192
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-12 08:55:04 -0500

    add: address model to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, Address
 
 class CustomUserAdmin(UserAdmin):
     model = get_user_model()
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+admin.site.register(Address)
