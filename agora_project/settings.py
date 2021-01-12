c6acdee652e17999844d09834505be9c5ed01648
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-11 22:43:01 -0500

    fix: admin site working

--- agora_project/settings.py
+++ agora_project/settings.py
@@ -131,6 +131,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+	"django.contrib.staticfiles.finders.FileSystemFinder",
+	"django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+
 
 # Adding custom user model to Authentication
 AUTH_USER_MODEL = 'accounts.CustomUser'
