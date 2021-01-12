d61693152bee866a987eab94dfbe10b23a713dd7
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-12 12:16:07 -0500

    add: P_color model

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from .models import (Product, 
                     P_Category, 
-                    P_Image,)
+                    P_Image,
+                    P_Color)
 
-product_models = [Product, P_Category, P_Image]
+product_models = [Product, P_Category, P_Image, P_Color]
 admin.site.register(product_models)
--- products/models.py
+++ products/models.py
@@ -36,3 +36,8 @@ class P_Image(models.Model):
     image_url = models.URLField(max_length=400)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
+class P_Color(models.Model):
+    color_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    color_name = models.CharField(max_length=20)
+    color_code_hex = models.CharField(max_length=6, blank=True, null=True)
+    product = models.ManyToManyField(Product, related_name='p_color')
