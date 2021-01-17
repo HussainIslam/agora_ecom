c2a9fdd48665c8a751d5b8528d7f8ad07061e81b
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-17 14:22:13 -0500

    add: urls for shipper, order, orderline

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path, include
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    
+    path('shippers/', views.ShipperList.as_view(), name='shippers'),
+    path('shippers/<uuid:pk>/', views.ShipperDetail.as_view(), name='shipper'),
+    path('orders/', views.OrderList.as_view(), name='orders'),
+    path('orders/<uuid:pk>/', views.OrderDetail.as_view(), name='order'),
+    path('orderlines/', views.OrderlineList.as_view(), name='orderlines'),
+    path('orderlines/<uuid:pk>/', views.OrderlineDetail.as_view(), name='orderline'),
 ]
