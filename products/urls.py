ea17ec1fbe4ea05293a88e846c5c6ab950dd3f4e
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-17 12:40:10 -0500

    add: urls for p_brands

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('products/<uuid:pk>/', views.ProductDetail, name='product'),
     path('categories/', views.PCategoryList.as_view(), name='categories'),
     path('categories/<uuid:pk>/', views.PCategoryDetail.as_view(), name='category'),
+    path('brands/', views.PBrandList.as_view(), name='brands'),
+    path('brands/<uuid:key>/', views.PBrandDetail.as_view(), name='brand'),
 
 ]
