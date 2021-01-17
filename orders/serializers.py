cb910e0446a406fa456fbb04bbb2b85726a7873b
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-17 16:13:48 -0500

    fix: shipper,order, orderline serlizers have Meta now

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,13 +3,19 @@ from rest_framework import serializers
 from .models import Shipper, Order, Orderline
 
 class ShipperSerializer(serializers.ModelSerializer):
-    model = Shipper
-    fields = '__all__'
+    
+    class Meta:
+        model = Shipper
+        fields = '__all__'
 
 class OrderSerializer(serializers.ModelSerializer):
-    model = Order
-    fields = '__all__'
+
+    class Meta:
+        model = Order
+        fields = '__all__'
 
 class OrderlineSerializer(serializers.ModelSerializer):
-    model = Orderline
-    fields = '__all__'
+
+    class Meta:
+        model = Orderline
+        fields = '__all__'
