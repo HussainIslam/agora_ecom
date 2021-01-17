9786268d561b399e3043fa79de4f984918c4a4ed
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-17 14:09:34 -0500

    add: OrderSerializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -5,3 +5,7 @@ from .models import Shipper, Order, Orderline
 class ShipperSerializer(serializers.ModelSerializer):
     model = Shipper
     fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    model = Order
+    fields = '__all__'
