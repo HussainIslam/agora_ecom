208aadb7f91e0864010919738c3265e15bd2dfc3
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-17 12:02:28 -0500

    add: crud+List views of product

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+from .views import ProductList, ProductDetail
 
 urlpatterns = [
-    
+    path('', ProductList.as_view(), name='products'),
+    path('<uuid:pk>/', ProductDetail, name='product')
 ]
