0b5c5c3ee2b55f010f5bd73452dffea699e8b492
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-21 23:10:47 -0500

    add: comments in brand model test, mod: moved testing object from setup to setuptestdata

--- products/tests/test_p_brand_model.py
+++ products/tests/test_p_brand_model.py
@@ -10,29 +10,37 @@ class BrandModelTestClass(TestCase):
         '''
         Setup Test Data for Brand's model
         '''
-        P_Brand.objects.create(
+        cls.brand = P_Brand.objects.create(
             brand_name="Test",
             brand_description="Test Description",
             brand_website="http://example.com/"
         )
 
-    def setUp(self):
-        
-        self.brand = P_Brand.objects.get(brand_name="Test")
-
     def test_brand_name_label(self):
+        '''
+        Testing the label of brand_name
+        '''
         name_label = self.brand._meta.get_field("brand_name").verbose_name
         self.assertEqual(name_label, 'brand name')
 
     def test_brand_description_label(self):
+        '''
+        Testing the label of brand_description
+        '''
         description_label = self.brand._meta.get_field("brand_description").verbose_name
         self.assertEqual(description_label, 'brand description')
 
     def test_brand_logo_label(self):
+        '''
+        Testing the label of brand_logo
+        '''
         logo_label = self.brand._meta.get_field("brand_logo").verbose_name
         self.assertEqual(logo_label, 'brand logo')
 
     def test_brand_website_label(self):
+        '''
+        Testing the label of website
+        '''
         website_label = self.brand._meta.get_field("brand_website").verbose_name
         self.assertEqual(website_label, 'brand website')
 
