a461abc0fe7fa84e95bb66fb0c8b328f039bb555
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-22 09:57:42 -0500

    mod: cart model; add: cartitem model

--- carts/models.py
+++ carts/models.py
@@ -6,11 +6,18 @@ from accounts.models import CustomUser
 
 class Cart(models.Model):
     cart_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    quantity = models.FloatField(default=0.00)
     creation_time = models.DateTimeField(auto_now_add=True)
     modification_time = models.DateTimeField(auto_now=True)
-    products = models.ManyToManyField(Product, related_name='product_cart')
     customer = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='customer_cart')
 
     def __str__(self):
         return self.customer
+
+class CartItem(models.model):
+    cartitem_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    quantity = models.FloatField(default=0.00)
+    added_time = models.DateTimeField(auto_now_add=True)
+    modification_time = models.DateTimeField(auto_now=True)
+    products = models.ManyToManyField(Product, related_name='product_cart')
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_cartitem')
+    
