478b936e7e8020ef593a9b96baf18f5c90054d9a
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-22 22:43:18 -0500

    mod: model of size to include get_label

--- products/tests/test_p_size_model.py
+++ products/tests/test_p_size_model.py
@@ -17,12 +17,20 @@ class SizeModelTestClass(TestCase):
         )
     
     def get_label(self, field_name):
-        return self.product._meta.get_field(field_name).verbose_name
+        '''
+        Method to extract verbose name from field name
+        '''
+        return self.size._meta.get_field(field_name).verbose_name
     
     def test_value_label(self):
-        value_label = self.size._meta.get_field('value').verbose_name
-        self.assertEqual(value_label, 'value')
+        '''
+        Testing label of value field
+        '''
+        self.assertEqual(self.get_label('value'), 'value')
 
     def test_size_category(self):
+        '''
+        Testing whether size is pointing to the correct category
+        '''
         self.assertEqual(self.categ.category_id, self.size.category.category_id)
         self.assertEqual(self.categ, self.size.category)
