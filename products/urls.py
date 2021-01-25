e839e2f282a5f6f31870c7083e8edb8b99610c65
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-25 12:44:57 -0500

    fix: products url to include details view

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductList.as_view(), name='products'),
-    path('products/<uuid:pk>/', views.ProductDetail, name='product'),
+    path('products/<uuid:pk>/', views.ProductDetail.as_view(), name='product'),
     path('categories/', views.PCategoryList.as_view(), name='categories'),
     path('categories/<uuid:pk>/', views.PCategoryDetail.as_view(), name='category'),
     path('brands/', views.PBrandList.as_view(), name='brands'),
