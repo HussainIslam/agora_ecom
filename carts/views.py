8877fb0c8567ebb40892ef22c012169412e3e152
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-25 12:33:26 -0500

    add: url for cartitem

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 
-from .models import Cart
-from .serializers import CartSerializer
+from .models import Cart, CartItem
+from .serializers import CartSerializer, CartItemSerializer
 
 class CartList(generics.ListCreateAPIView):
     queryset = Cart.objects.all()
@@ -11,4 +11,10 @@ class CartDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
 
+class CartItemList(generics.ListCreateAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
 
+class CartItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
