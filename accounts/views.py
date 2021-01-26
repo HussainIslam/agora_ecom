5feb56210d5f1898dd433eeaca020e2c48a4a3ae
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-26 10:37:30 -0500

    fix: #3 both List and Detail view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Address
+from .serializers import AddressSerializer
+
+class AddressList(generics.ListCreateAPIView):
+    model = Address
+    serializer_class = AddressSerializer
+
+class AddressDetail(generics.RetrieveUpdateDestroyAPIView):
+    model = Address
+    serializer_class = AddressSerializer
