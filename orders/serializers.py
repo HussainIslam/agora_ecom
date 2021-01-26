28c80e3c0a1ae5751ba3a1455d700d4c13e6050b
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-26 10:16:43 -0500

    add: hypermedia for order

--- orders/serializers.py
+++ orders/serializers.py
@@ -10,13 +10,16 @@ class ShipperSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class OrderSerializer(serializers.ModelSerializer):
+    shipper = serializers.HyperlinkedRelatedField(read_only=True, view_name='shipper')
+    billing_address = serializers.HyperlinkedRelatedField(read_only=True, view_name='address')
+    shipping_address = serializers.HypelinkedRelatedField(read_only=True, view_name='address')
 
     class Meta:
         model = Order
         fields = '__all__'
 
 class OrderlineSerializer(serializers.ModelSerializer):
-
+    
     class Meta:
         model = Orderline
         fields = '__all__'
