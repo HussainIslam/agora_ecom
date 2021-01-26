1854c90b4008cb24671c44051ffe2e524bc2a4f7
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-26 10:45:40 -0500

    fix: #5

--- carts/urls.py
+++ carts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.CartList.as_view(), name='carts'),
     path('<uuid:pk>/', views.CartDetail.as_view(), name='cart'),
     path('cartitems/', views.CartItemList.as_view(), name='cartitems'),
-    path('cartitems/<uuid:pk/', views.CartItemDetail.as_view(), name='cartitem')
+    path('cartitems/<uuid:pk>/', views.CartItemDetail.as_view(), name='cartitem')
 ]
