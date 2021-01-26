7d594c8e2d08a2dd2d0fdd93be09411a6756e3d5
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-01-26 10:33:01 -0500

    fix: #4 serializer for address

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from .models import CustomUser
+from .models import CustomUser, Address
 
 class CustomUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
+        fields = '__all__'
+
+class AddressSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Address
         fields = '__all__'
