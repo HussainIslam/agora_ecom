1b2b90e2dae3ee2982cc35c67b7b9d6ffde9c114
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-02-05 22:43:32 -0500

    fix: #19

--- carts/serializers.py
+++ carts/serializers.py
@@ -11,6 +11,9 @@ class CartSerializer(serializers.ModelSerializer):
 
 class CartItemSerializer(serializers.ModelSerializer):
     cart = serializers.HyperlinkedRelatedField(view_name='cart', read_only=True)
+    product = serializers.HyperlinkedRelatedField(view_name='product', read_only = True)
+    color = serializers.HyperlinkedRelatedField(view_name='color', read_only = True)
+    size = serializers.HyperlinkedRelatedField(view_name='size', read_only = True)
 
     class Meta:
         model = CartItem
