0ed18ed85ad568106c5b57876d48c0aacefb7cae
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-02-16 12:44:08 -0500

    fix #22

--- carts/models.py
+++ carts/models.py
@@ -1,14 +1,14 @@
 import uuid
 from django.db import models
+from django.contrib.auth import get_user_model
 
 from products.models import Product, P_Color, P_Size
-from accounts.models import CustomUser
 
 class Cart(models.Model):
     cart_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     creation_time = models.DateTimeField(auto_now_add=True)
     modification_time = models.DateTimeField(auto_now=True)
-    customer = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='customer_cart')
+    customer = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name='customer_cart')
 
     def __str__(self):
         return self.customer
