1044b7bebc72e5a59df19b7c7b00806361daac45
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-02-17 22:13:38 -0500

    fix #24

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Shipper, Order, Orderline
+from .models import Shipper, Order, Orderline, Promotion, Payment
 
-order_models = [Shipper, Order, Orderline]
+order_models = [Shipper, Order, Orderline, Promotion, Payment]
 
 admin.site.register(order_models)
