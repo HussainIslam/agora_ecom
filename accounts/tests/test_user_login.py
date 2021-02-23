be8fa6c0403365b2f7938ca52935aad96e6d891c
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2021-02-23 13:59:16 -0500

    add: added some login tests

---
+++ accounts/tests/test_user_login.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+class LoginTestClass(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.email = "testuser@gmail.com"
+        cls.password = "Password123!"
+        cls.userobject = {
+            "email": cls.email,
+            "password": cls.password
+        }
+        cls.reg_url = reverse('accounts:registration')
+        cls.login_url = reverse('accounts:login')
+        cls.resolver = resolve('/users/login/')
+    
+    def setUp(self):
+        self.response = self.client.post(path=self.reg_url, data=self.userobject, content_type="application/json")
+
+    def test_user_login_url(self):
+        self.assertEqual(self.login_url, '/users/login/')
+
+    def test_user_login_url_name(self):
+        self.assertEqual(self.resolver.url_name, 'login')
+
+    def test_get_wrong_status_code(self):
+        response = self.client.get(path=self.login_url)
+        self.assertNotEqual(response.status_code, 200)
+    
+    def test_get_correct_status_code(self):
+        response = self.client.get(path=self.login_url)
+        self.assertEqual(response.status_code, 405)
+    
+    def test_post_correct_status_code(self):
+        req_data = {
+            'email': self.email, 
+            'password': self.password
+        }
+        login_response = self.client.post(path=self.login_url, data=req_data, content_type='application/json')
+        print(login_response.status_code)
--- accounts/tests/test_user_registration.py
+++ accounts/tests/test_user_registration.py
@@ -9,46 +9,19 @@ class UserRegistrationTestClass(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.billing_address = Address.objects.create(
-            street_address= "30 Denton Avenue",
-            city= "Toronto",
-            province= "ON",
-            postal_code= "M1L 4P2",
-            country= "Canada"
-        )
-
-        cls.shipping_address = Address.objects.create(
-            street_address= "10 Teesdale Place",
-            city= "Scarborough",
-            province= "ON",
-            postal_code= "M1P 4E3",
-            country= "Canada"
-        )
-
-        cls.customer = CustomUser.objects.create_user(
-            first_name= "User",
-            last_name= "One",
-            username= "user1",
-            date_of_birth= "2012-09-15",
-            billing_address=cls.billing_address,
-            shipping_address=cls.shipping_address,
-            phone= "444-555-6666",
-            email= "user1@gmail.com",
-            password= "123456",
-            gender= "Male",
-            registered_on= "2012-09-15T22:17:44+00:00",
-            last_modified= "2012-09-15T22:17:44+00:00",
-            is_active= True,
-            is_staff= False
-        )
         cls.url = reverse('accounts:registration')
         cls.resolver = resolve('/users/')
+        cls.email = "testuser@gmail.com"
+        cls.password = "Password123!"
         cls.userobject = {
-            "email": "testuser@gmail.com",
-            "password": "Password123!"
+            "email": cls.email,
+            "password": cls.password
         }
 
-    def test_user_registration(self):
+    def setUp(self):
+        self.response = self.client.post(path=self.url, data=self.userobject, content_type='application/json')
+
+    def test_user_registration_url(self):
         self.assertEqual(self.url, '/users/')
     
     def test_registration_url_name(self):
@@ -63,17 +36,17 @@ class UserRegistrationTestClass(TestCase):
         self.assertEqual(response.status_code, 405)
 
     def test_post_correct_status_code(self):
-        response = self.client.post(path=self.url, data=self.userobject, content_type='application/json')
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(self.response.status_code, 201)
     
     def test_post_response_email(self):
-        response = self.client.post(path=self.url, data=self.userobject, content_type='application/json')
-        self.assertEqual(response.data['email'], self.userobject['email'])
+        self.assertEqual(self.response.data['email'], self.userobject['email'])
     
     def test_post_response_password(self):
-        response = self.client.post(path=self.url, data=self.userobject, content_type='application/json')
-        self.assertNotEqual(response.data['password'], self.userobject['password'])
+        self.assertNotEqual(self.response.data['password'], self.userobject['password'])
 
     def test_post_response_no_password(self):
-        response = self.client.post(path=self.url, data=self.userobject, content_type='application/json')
-        self.assertNotContains(response.data, 'password')
+        self.assertNotContains(self.response.data, 'password')
+
+    def test_user_created(self):
+        user = CustomUser.objects.get(email=self.email)
+        self.assertEqual(user.email, self.email)
